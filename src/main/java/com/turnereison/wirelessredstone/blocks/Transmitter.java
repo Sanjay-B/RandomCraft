d007864b017b31242189486480114c28e91d32c7
Turner Eison <turner@turnereison.com>
2021-07-12 00:08:12 -0400

    TransmitterEntity

--- src/main/java/com/turnereison/wirelessredstone/blocks/Transmitter.java
+++ src/main/java/com/turnereison/wirelessredstone/blocks/Transmitter.java
@@ -1,23 +1,28 @@
 package com.turnereison.wirelessredstone.blocks;
 
+import com.turnereison.wirelessredstone.blocks.entities.TransmitterEntity;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class Transmitter extends Block {
+public class Transmitter extends Block implements BlockEntityProvider {
     public static final BooleanProperty TRANSMITTING = BooleanProperty.of("transmitting");
 
     public Transmitter(Settings settings) {
@@ -31,6 +36,11 @@ public class Transmitter extends Block {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         // toggle transmitting property
+
+        TransmitterEntity entity = (TransmitterEntity) world.getBlockEntity(pos);
+
+        entity.setTransmitting(!entity.getTransmitting());
+
         boolean transmitting = state.get(TRANSMITTING);
         world.setBlockState(pos, state.with(TRANSMITTING, !transmitting));
         return ActionResult.SUCCESS;
@@ -47,5 +57,17 @@ public class Transmitter extends Block {
                 Block.createCuboidShape(1,0,1,15,1,15),
                 Block.createCuboidShape(7,1,7, 9,12,9)
         );
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new TransmitterEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return TransmitterEntity::tick;
     }
 }
